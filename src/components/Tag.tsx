9aa4d566c83448a8cbdaaeed456a0134245259c9
Oksana Korobanova (Vitol) <58445023+OksanaKorobanova@users.noreply.github.com>
2024-10-04 18:53:35 +0200

    added block posts page

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,8 +1,8 @@
 import { type ComponentPropsWithoutRef } from 'react';
 
-type Props = ComponentPropsWithoutRef<'div'> & { color: string };
+type Props = ComponentPropsWithoutRef<'div'> & { color?: string };
 
-const Tag = ({ children, className, color }: Props) => {
+const Tag = ({ children, className, color = 'fuchsia' }: Props) => {
   return (
     <div
       className={`px-3 py-1.5 uppercase font-heading font-extrabold tracking-wider text-xs bg-${color}-500/15 text-${color}-500 inline-flex rounded-full ${className}`}>
